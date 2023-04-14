73490ce01cc6dade47b19a1cf40ac221a35a7d24
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-14 13:19:19 +0300

    constructor

---
+++ FoodItem.java
@@ -0,0 +1,17 @@
+public class FoodItem{
+    public String name;
+
+    //constructor
+    FoodItem(String name){
+        this.name = name;//this keyword
+    }
+
+    private String displayPrice(boolean highPrice){
+
+    }
+
+    public static void main(String[] args) {
+        //Object Using Constructor
+        FoodItem foodItem = new FoodItem("Apple");
+    }
+}
